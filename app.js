e1182502a7698e3e1312cba931feaf9118347ec4
Yash86400-design <knowledge86400@gmail.com>
2023-01-03 13:07:16 +0530

    Finally able to retrieve form data inside my js file, damn I thought it would be easy but hell no

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 import { data } from "./countriesList.js";
 
-const countries = data();
-console.log(countries);
+const countries = data;
+
+const button = document.querySelector("#submitButton");
+button.addEventListener("click", dataGather)
+
+function dataGather (event) {
+  event.preventDefault()
+  const country = document.querySelector("#country").value;
+  const firstDate = document.querySelector("#firstDate").value;
+  const lastDate = document.querySelector("#lastDate").value;
+
+  console.log(country, firstDate, lastDate);
+}
