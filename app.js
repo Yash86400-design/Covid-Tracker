c2a0556dbd318b680a86d999d407b19f79f59cae
Yash86400-design <knowledge86400@gmail.com>
2023-01-06 12:38:03 +0530

    Finally created the basic structure of our website, I'm getting Slug value back, both dates. I'm so happy

--- app.js
+++ app.js
@@ -1,53 +1,52 @@
 window.onload = function insertingCountryList() {
-
-  // let country = getData();
-  // let option = document.createElement("option");
-
-  // const countryList = document.querySelector("#country");
-  // countryList.appendChild(createSelectItem());
-
-
-  // console.log(country);
-  insertCountriesInSelectBar()
+  if ("array" in localStorage && "object" in localStorage) {
+    const countriesArray = JSON.parse(localStorage.getItem("array"))
+    const countriesObject = JSON.parse(localStorage.getItem("object"))
+    createSelectItem(countriesArray, countriesObject)
+  } else {
+    fetchListOfCountries()
+    // console.log("I Hope You Are not get called");
+  }
 }
 
 const button = document.querySelector("#submitButton");
 button.addEventListener("click", dataGather);
 
 const countryList = document.querySelector("#country");
-// dropDown.addEventListener("click", (event) => { console.log(event); })
 
 function dataGather(event) {
   event.preventDefault()
-  const country = document.querySelector("#country").value;
+  const country = document.querySelector("select").value;
   const firstDate = document.querySelector("#firstDate").value;
   const lastDate = document.querySelector("#lastDate").value;
 
   console.log(country, firstDate, lastDate);
 };
 
-function insertCountriesInSelectBar() {
+function fetchListOfCountries() {
   fetch("https://api.covid19api.com/countries")
     .then((response) => response.json())
     .then(function (responseData) {
-      let countries = {}
-      // let countries = []
+      let countriesAndSlugObject = {}
+      let countries = []
 
       for (let country = 0; country < responseData.length; country++) {
-        countries[responseData[country]["Country"]] = responseData[country]["Slug"]
+        countriesAndSlugObject[responseData[country]["Country"]] = responseData[country]["Slug"]
+        countries.push(responseData[country]["Country"])
       }
-      createSelectItem(countries);
+      countries = countries.sort()
+      localStorage.setItem("array", JSON.stringify(countries))
+      localStorage.setItem("object", JSON.stringify(countriesAndSlugObject))
+      // createSelectItem(countries);
     });
 };
 
-function createSelectItem(data) {
-  console.log(typeof (data));
+function createSelectItem(data, objectOfSlug) {
+  data = data.sort()
   const select = document.querySelector("select")
-  for (const property in data) {
-    console.log(`${property}: ${data[property]}`);
-    select.appendChild(createOptionItem(property, data[property]))
+  for (let i = 0; i < data.length; i++) {
+    select.appendChild(createOptionItem(data[i], objectOfSlug[data[i]]))
   }
-
 }
 
 function createOptionItem(content, value) {
